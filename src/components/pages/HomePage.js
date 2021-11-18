b9d6ff223b7be957ce8f640b9ad4de3867ecc351
Iryna <irasokolova25@ukr.net>
2021-11-19 00:09:13 +0200

    create layout

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,3 +1,32 @@
 /** @format */
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useRouterMatch } from "react-router-dom";
+// import Container from "../Container/Container";
 
-export default function HomePage() {}
+export default function HomePage() {
+  const [movies, setMovie] = useState([]);
+  const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
+  const URL = "https://api.themoviedb.org/3/";
+  console.log(movies);
+
+  useEffect(() => {
+    axios
+      .get(`${URL}trending/all/week?api_key=${KEY}`)
+      .then((r) => r.data)
+      .then((data) => setMovie(data.results));
+  }, []);
+  return (
+    <>
+      <h1>Tranding Today</h1>
+      <ul>
+        {movies &&
+          movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`${movie.id}`}>{movie.title}</Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
