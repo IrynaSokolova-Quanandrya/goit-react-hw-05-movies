d733c2713a1a3cb2f6bd8ac045d5abec559f677c
Iryna <irasokolova25@ukr.net>
2021-11-19 00:06:57 +0200

    create loyaut

--- src/components/pages/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage.js
@@ -1,3 +1,17 @@
 /** @format */
 
-export default function MovieDetailsPage() {}
+export default function MovieDetailsPage() {
+  return (
+    <div>
+      <img className='img'>img</img>
+      <h1>title</h1>
+      <p>User Score:</p>
+      <h2>Overview</h2>
+      <p>Overview: text</p>
+      <h3>Genres</h3>
+      <ul>
+        <li>genres</li>
+      </ul>
+    </div>
+  );
+}
