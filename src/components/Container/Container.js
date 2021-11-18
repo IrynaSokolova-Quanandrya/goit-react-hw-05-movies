3a841b8c7c002e6ca47986d4b20353f7cbcfba4a
Iryna <irasokolova25@ukr.net>
2021-11-19 00:09:35 +0200

    Create Container.js

---
+++ src/components/Container/Container.js
@@ -0,0 +1,9 @@
+/** @format */
+
+// /** @format */
+
+// import s from "../Container/Container.module.css";
+
+// export default function Container() {
+//   return <div className={s.Container}></div>;
+// }
