85b7db1ef278075159454df1e514b5307fc9b89c
Iryna <irasokolova25@ukr.net>
2021-11-21 12:35:43 +0200

    Update HomePage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,20 +1,21 @@
 /** @format */
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link, useRouterMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import Container from "../Container/Container";
 
 export default function HomePage() {
-  const [movies, setMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
+
   const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
   const URL = "https://api.themoviedb.org/3/";
-  console.log(movies);
+  // console.log(movies);
 
   useEffect(() => {
     axios
       .get(`${URL}trending/all/week?api_key=${KEY}`)
       .then((r) => r.data)
-      .then((data) => setMovie(data.results));
+      .then((data) => setMovies(data.results));
   }, []);
   return (
     <>
@@ -23,7 +24,7 @@ export default function HomePage() {
         {movies &&
           movies.map((movie) => (
             <li key={movie.id}>
-              <Link to={`${movie.id}`}>{movie.title}</Link>
+              <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
             </li>
           ))}
       </ul>
