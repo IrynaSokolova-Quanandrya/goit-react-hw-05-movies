59a1b73f461869153f54cf85f21b883a7ab9a7db
Iryna <irasokolova25@ukr.net>
2021-11-27 00:17:52 +0200

    Update MovieDetailsPage.js

--- src/components/pages/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage.js
@@ -1,35 +1,60 @@
 /** @format */
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-import { useParams, useMatch } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
+// import {BrowserHistory} from 'react-router'
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Cast from './Cast';
+// import Reviews from './Reviews';
 
 export default function MovieDetailsPage() {
-  // const [movie, setMovie] = useState([]);
-console.log(useMatch());
-  // const match = useMatch();
-  // console.log(match);
-  // const { movieId } = useParams();
+  const [movie, setMovie] = useState([]);
+// console.log(movie);
+  const { movieId } = useParams();
   // console.log({ movieId });
-  // const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
-  // const URL = "https://api.themoviedb.org/3/";
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`${URL}trending/all/week?api_key=${KEY}`)
-  //     .then((r) => r.data)
-  //     .then((movie) => console.log(movie.results.id));
-  // }, []);
+  const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
+  const URL = "https://api.themoviedb.org/3/";
+  
+
+  useEffect(() => {
+    axios
+      .get(`${URL}/movie/${movieId}?api_key=${KEY}&language=en-US`)
+      .then((r) =>(r.data))
+      .then((movie) =>setMovie (movie))
+    }, []);
   return (
+    
     <div>
-      {/* <img className='img'>img</img> */}
-      {/* <h1>{`Books ${movieId}`}</h1> */}
-      <p>User Score:</p>
-      <h2>Overview</h2>
-      <p>Overview: text</p>
-      <h3>Genres</h3>
+      {/* <button onClick={BrowserHistory.goBack}>Go Back</button> */}
+            <img 
+            src ={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+            alt = {`${movie.original_title}`} 
+            className='img'/>
+        <h1>{`Books ${movie.original_title}`}</h1>
+        <p>User Score: {`${movie.vote_average}`}</p>
+        <h2>Overview</h2>
+        <p>{`${movie.overview}`}</p>
+        <h3>Genres</h3>
+        <ul>
+           {/* {movie && movie.genres.map(genre=>{ 
+            <li>{`${genre.name}`}</li>
+          })} */}
+        </ul>
+        <p>Additional Information</p>
       <ul>
-        <li>genres</li>
+        <li>
+        <NavLink to={`/movies/${movieId}/cast`}>Cast</NavLink> 
+        </li>
+        <li>
+      <NavLink to={`/movies/${movieId}/reviews`}>Reviews</NavLink>
+        </li>
       </ul>
+      <Routes>
+      <Route path='/movies/:movieId/cast' element={<Cast />}/>
+      {/* <Route path={`/movies/${movieId}/reviews`} element={<Reviews />} /> */}
+      </Routes>
     </div>
   );
 }
