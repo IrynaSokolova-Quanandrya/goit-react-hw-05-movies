36b735caef47f165e985bb4eb43335b65bc19b27
Iryna <irasokolova25@ukr.net>
2021-11-27 23:49:03 +0200

    Update Cast.js

--- src/components/pages/Cast.js
+++ src/components/pages/Cast.js
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 
 function Cast(){
     const [movie, setMovie] = useState([]);
-
+console.log(movie);
     const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
     const URL = "https://api.themoviedb.org/3/";
     const { movieId } = useParams();
@@ -15,7 +15,7 @@ function Cast(){
     useEffect(()=>{
         axios
         .get(`${URL}movie/${movieId}/credits?api_key=${KEY}&language=en-US`)
-        .then(r=>console.log)
+        .then(r=>setMovie(r.data))
     })
     return <>
     {/* <button onClick={BrowserHistory.goBack}>Go Back</button> */}
