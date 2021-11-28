a666a4a1c7eaa54193142206a9de13c7f96cd14f
Iryna <irasokolova25@ukr.net>
2021-11-28 22:44:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import "./App.module.css";
 import Container from "./components/Container/Container";
 import AppBar from "./components/AppBar/AppBar";
 import HomePage from "./components/pages/HomePage";
-// import MovieDetailsPage from "./components/pages/MovieDetailsPage";
-// import MoviesPage from "./components/pages/MoviesPage";
-import NotFoundPage from "./components/pages/NotFoundPage";
 
 const MoviesPage = lazy(()=> import('./components/pages/MoviesPage'));
 const MovieDetailsPage = lazy(()=> import('./components/pages/MovieDetailsPage'))
-
+const Cast = lazy(()=>import('./components/pages/Cast'));
+const Reviews = lazy(()=>import('./components/pages/Reviews'))
 
 export default function App() {
   return (
@@ -23,7 +21,8 @@ export default function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/movies' element={<MoviesPage />} />
         <Route path='/movies/:movieId' element={<MovieDetailsPage />} />
-        <Route element={<NotFoundPage />} />
+        <Route path='/movies/:movieId/cast' element={<Cast />} />
+        <Route path='/movies/:movieId/reviews' element={<Reviews />} />
       </Routes>
       </Suspense>
     </Container>
