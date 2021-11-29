69be1799c7b2446c87781c2d6507d3f692077f8a
Iryna <irasokolova25@ukr.net>
2021-11-29 15:52:35 +0200

    add key and fetch

--- src/components/pages/Cast.js
+++ src/components/pages/Cast.js
@@ -1,7 +1,8 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import s from '../Styles/Cast.module.css';
+import FetchCredits from "../../services/FetchApi";
+
 
 
 
@@ -9,13 +10,9 @@ function Cast(){
     const [credits, setCredits] = useState([]);
     const { movieId } = useParams();
     const navigate = useNavigate();
-    const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
-    const URL = "https://api.themoviedb.org/3/";
-console.log(credits);
+
     useEffect(()=>{
-        axios
-        .get(`${URL}movie/${movieId}/credits?api_key=${KEY}&language=en-US`)
-        .then(response=>response.data)
+        FetchCredits.FetchCastApi(movieId)
         .then(credits=>setCredits(credits.cast))
     },[movieId])
     return( 
@@ -24,7 +21,7 @@ console.log(credits);
     {credits &&
     <ul className={s.cast__list}>
         {credits.map(credit=>
-        <li>
+        <li key={credit.id}>
              <img
           src={`https://image.tmdb.org/t/p/w500${credit.profile_path}`}
           alt={`${credit.name}`}
@@ -34,9 +31,6 @@ console.log(credits);
         </li>)}
     
     </ul>}
-    
-    
-
     </>
     )  
 }
