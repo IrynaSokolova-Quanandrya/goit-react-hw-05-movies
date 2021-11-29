ce1038f997e65a74dc90ee81309d612f1b535334
Iryna <irasokolova25@ukr.net>
2021-11-29 15:53:40 +0200

    add fetch for Cast and Reviews

--- src/services/FetchApi.js
+++ src/services/FetchApi.js
@@ -1,10 +1,27 @@
 import axios from "axios";
 // import { useState, useEffect } from "react";
-
-export default function FetchApi() {
-    const URL = "https://api.themoviedb.org/3/";
+ const URL = "https://api.themoviedb.org/3/";
     const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
-    axios
+ function FetchHomeApi() {
+     return axios
     .get(`${URL}trending/all/week?api_key=${KEY}`)
     .then((r) => r.data)
+   
 }
+function FetchCastApi(movieId) {
+     return  axios
+     .get(`${URL}movie/${movieId}/credits?api_key=${KEY}&language=en-US`)
+     .then(r=>r.data)
+   
+}
+function FetchReviewsApi(movieId) {
+    return axios
+    .get(`${URL}movie/${movieId}/reviews?api_key=${KEY}&language=en-US`)
+    .then(r=>r.data)
+}
+const api = {
+    FetchHomeApi,
+    FetchCastApi,
+    FetchReviewsApi
+};
+export default api
