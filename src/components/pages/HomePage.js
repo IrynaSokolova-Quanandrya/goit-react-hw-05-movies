0a9236f6c68b20e995067cc5f0b59ce8a9c8461d
Iryna <irasokolova25@ukr.net>
2021-11-29 15:52:56 +0200

    add  fetch

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,18 +1,13 @@
 /** @format */
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import FetchMovies from "../../services/FetchApi";
 
 export default function HomePage() {
   const [movies, setMovies] = useState([]);
 
-  const KEY = "098c0a06f6f788991ea9bd1b1a28f1b9";
-  const URL = "https://api.themoviedb.org/3/";
-
   useEffect(() => {
-    axios
-      .get(`${URL}trending/all/week?api_key=${KEY}`)
-      .then((r) => r.data)
+    FetchMovies.FetchHomeApi()
       .then((data) => setMovies(data.results));
   }, []);
   return (
