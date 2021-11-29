a05c00a0631c5efe8a1201f0f3fbd8bc9ae5969a
Iryna <irasokolova25@ukr.net>
2021-11-29 17:52:03 +0200

    change nested routes

--- src/App.js
+++ src/App.js
@@ -20,9 +20,10 @@ export default function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/movies' element={<MoviesPage />} />
-        <Route path='/movies/:movieId/*' element={<MovieDetailsPage />} />
+        <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
         <Route path='/movies/:movieId/cast' element={<Cast />} />
         <Route path='/movies/:movieId/reviews' element={<Reviews />} />
+        </Route>
       </Routes>
       </Suspense>
     </Container>
