cb08c6785c3b12a00f38847dae89c863af411170
Iryna <irasokolova25@ukr.net>
2021-11-29 15:52:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/movies' element={<MoviesPage />} />
-        <Route path='/movies/:movieId' element={<MovieDetailsPage />} />
+        <Route path='/movies/:movieId/*' element={<MovieDetailsPage />} />
         <Route path='/movies/:movieId/cast' element={<Cast />} />
         <Route path='/movies/:movieId/reviews' element={<Reviews />} />
       </Routes>
